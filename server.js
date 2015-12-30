17fafd2a9a11186677867c1fc0ae8ed62fe66455
Hack Reactor Students <accounts@hackreactor.com>
2015-12-30 15:40:18 -0800

    add changes for deployment environment

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+var dotenv = require('dotenv').load();
 var app = require('./server-config.js');
 
-var port = 4568;
+// var port = 4568;
+// process.env.PORT = process.argv[2] || 4568;
 
-app.listen(port);
+// console.log(process.env.PORT);
 
-console.log('Server now listening on port ' + port);
+app.listen(process.env.PORT);
+
+console.log('Server now listening on port ' + process.env.PORT);
