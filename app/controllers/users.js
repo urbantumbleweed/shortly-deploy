16da9f288db03e401aecdb16d887f1bcef49577a
Hack Reactor Students <accounts@hackreactor.com>
2015-12-30 22:42:02 -0800

    started on mongoose implementation

---
+++ app/controllers/users.js
@@ -0,0 +1,14 @@
+var db = require('../config.js');
+var model = require('../models/user.js')
+
+var createUser = function(req, res) {
+  // new User({ username: req.username, password:  });
+}
+
+var readUser = function(req, res) {
+
+}
+
+var updateUser = function(req, res) {}
+
+var deleteUser = function(req, res) {}
